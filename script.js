a4093ceedcda8e7a6b3360cf4e119e50c660461a
Chirag Kapil <your.email@example.com>
2024-08-17 19:54:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let apiKey = "2p8lpMpkI-VxEK1Vy0EOfQE646wmysvZLMGztoVpLq8";
+let count = 10;
+
+let apiUrl = `https:api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+console.log(apiUrl);
+
+async function getPhotos() {
+
+  let response = await fetch(apiUrl);
+  let data = await response.json();
+  console.log(data);
+
+  let box = document.querySelector(".box");
+  data.forEach((photo) => {
+    let img = document.createElement("img");
+    img.src = photo.urls.regular;
+    img.style.height = "450px";
+    img.style.width = "400px";
+    box.appendChild(img);
+  });
+}
+
+getPhotos();
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY + window.innerHeight+100 >= document.body.offsetHeight) {
+    getPhotos();
+  }
+});

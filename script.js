651a2ebe60ebfe0cd4f9641085a792fe989683bf
Chirag Kapil <your.email@example.com>
2024-08-20 13:30:20 +0530

    comitting try n catch

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let apiKey = "DUbOaOakxcTWF5daWI4c2A9jKVDO94tG9fkA8RugZEM";
 
 let count = 20;
 
-async function getPhotos() {
+// https:api.unsplash.com/photos/random/?client_id=DUbOaOakxcTWF5daWI4c2A9jKVDO94tG9fkA8RugZEM&count=10
 
-  let response = await fetch(`https:api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`);
+async function getPhotos() {
+  try {
+    let response = await fetch(`https:api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`);
   let data = await response.json();
   console.log(data);
 
@@ -16,12 +18,17 @@ async function getPhotos() {
     img.style.width = "400px";
     box.appendChild(img);
   });
+  } catch (error) {
+    console.log(error);
+    
+  }
+
 }
 
 getPhotos();
 
 window.addEventListener("scroll", function () {
-  if (window.scrollY + window.innerHeight + 100 >= document.body.offsetHeight) {
+  if (window.scrollY + window.innerHeight + 100 >= document.body.offsetHeight){
     getPhotos();
   }
 });

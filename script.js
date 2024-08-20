6658028dce8b18655122a639e4c247f3cf46d83b
Chirag Kapil <your.email@example.com>
2024-08-20 13:43:43 +0530

    comitting change in api

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let apiKey = "DUbOaOakxcTWF5daWI4c2A9jKVDO94tG9fkA8RugZEM";
+// let apiKey = "45496333-3fa1d67e86f676313d5f86f45";
 
-let count = 20;
+// let count = 20;
 
 // https:api.unsplash.com/photos/random/?client_id=DUbOaOakxcTWF5daWI4c2A9jKVDO94tG9fkA8RugZEM&count=10
 
 async function getPhotos() {
   try {
-    let response = await fetch(`https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`);
+    let response = await fetch(`https://pixabay.com/api/?key=45496333-3fa1d67e86f676313d5f86f45&q=yellow+flowers&image_type=photo`);
   let data = await response.json();
   console.log(data);
 
   let box = document.querySelector(".box");
-  data.forEach((photo) => {
+  data.hits.forEach((photo) => {
     let img = document.createElement("img");
-    img.src = photo.urls.regular;
+    img.src = photo.largeImageURL;
     img.style.height = "450px";
     img.style.width = "400px";
     box.appendChild(img);

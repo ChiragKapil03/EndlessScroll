f97f5e699254f3ef40b89d6da0cd194b0a1e37ad
Chirag Kapil <your.email@example.com>
2024-08-21 18:48:41 +0530

    last commit

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
-
-function randomHeight(){
-  let a = Math.floor(300+Math.random()*450)+"px";
-  return a;
-}
-function randomWidth(){
-  let a = Math.floor(300+Math.random()*450)+"px";
-  return a;
+function randomSize() {
+  let size = Math.floor(350 + Math.random() * 100) + "px";
+  return size;
 }
 
 async function getPhotos() {
   try {
-    let response = await fetch(`https://pixabay.com/api/?key=45496333-3fa1d67e86f676313d5f86f45&q=galaxy&image_type=photo`);
-  let data = await response.json();
-  console.log(data);
+      let response = await fetch(`https://pixabay.com/api/?key=45496333-3fa1d67e86f676313d5f86f45&q=galaxy&image_type=photo`);
+      let data = await response.json();
+      console.log(data);
 
-  let box = document.querySelector(".box");
-  data.hits.forEach((photo) => {
-    let img = document.createElement("img");
-    img.src = photo.largeImageURL;
-    img.style.height = randomHeight();
-    img.style.width = randomWidth();
-    box.appendChild(img);
-  });
+      let box = document.querySelector(".box");
+      data.hits.forEach((photo) => {
+          let img = document.createElement("img");
+          img.src = photo.largeImageURL;
+          img.style.height = randomSize();
+          img.style.width = randomSize();
+          box.appendChild(img);
+      });
   } catch (error) {
-    console.log(error);
-    
+      console.log(error);
   }
-
 }
 
 getPhotos();
 
 window.addEventListener("scroll", function () {
-  if (window.scrollY + window.innerHeight + 100 >= document.body.offsetHeight){
-    getPhotos();
+  if (window.scrollY + window.innerHeight + 100 >= document.body.offsetHeight) {
+      getPhotos();
   }
 });
